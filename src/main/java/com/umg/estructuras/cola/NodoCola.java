94c92648dc586617850d31968774415e258a1a6c
codeflow1958 <nickylds1958@gmail.com>
2025-05-21 00:39:51 -0600

    agregamos nodo

--- src/main/java/com/umg/estructuras/cola/NodoCola.java
+++ src/main/java/com/umg/estructuras/cola/NodoCola.java
@@ -1,4 +1,18 @@
 package com.umg.estructuras.cola;
 
-public class NodoCola {
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class NodoCola<T> {
+    private static final Logger LOGGER = Logger.getLogger(NodoCola.class.getName());
+
+    public T dato;
+    public NodoCola<T> siguiente;
+
+    public NodoCola(T dato) {
+        this.dato = dato;
+        this.siguiente = null;
+        LOGGER.log(Level.FINE, "NodoCola creado con dato: {0}", dato);
+    }
 }
+
